6b1f849f409c64586f8fb8c51c2920b01f4abd78
dikagfani <ardikaghathfani@gmail.com>
2026-07-12 20:23:02 +0700

    feat:update colors palette and Navbar

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -1,23 +1,23 @@
 import Link from "next/link"
-import { MessageCircle, ArrowDown } from "lucide-react";
+import { MessageCircle, ArrowDown, ArrowRight } from "lucide-react";
 import Badge from "../ui/Badge"
 import Button from "../ui/Button"
 
 export default function Hero() {
   return (
-    <section className="relative overflow-hidden px-4 pt-16 pb-24 text-center md:pt-24">
+    <section className="relative overflow-hidden px-4 pt-16 pb-24 text-center md:pt-20">
       <div className=" mx-auto flex max-w-3xl flex-col items-center gap-8">
-        <Badge color="yellow" withDot>
+        <Badge color="yellow" withDot classname="shadow-[4px_4px_0px_var(--color-ink)]">
           AVAILABLE FOR NEW PROJECTS
         </Badge>
 
-        <h1 className="font-display text-5xl font-bold leading-[1.05] sm:text-6xl md:text-8xl">
+        <h1 className="font-display text-5xl font-black leading-[0.9] sm:text-6xl md:text-8xl">
           BEYOND
           <br />
           <span className="inline-block rotate-2 text-blue">THE USUAL</span>
           <br />
           <mark className="inline-block rounded-lg bg-pink px-3 text-surface -rotate-2 shadow-[4px_4px_0px_var(--color-ink)] ">
-            PIXEL
+            PIXEL.
           </mark>
         </h1>
 
@@ -28,7 +28,7 @@ export default function Hero() {
         <div className="flex flex-col gap-4 sm:flex-row">
           <a href="#projects">
           <Button variant="secondary" className="gap-2">
-            Explore Work
+            Explore Work <ArrowRight size={16} />
           </Button>
           </a>
           <a href="#contact">
